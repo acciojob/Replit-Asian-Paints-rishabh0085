3eebc4aa9e5349e995141a434d797a63e49373da
Rishabh Singh <52191143+rishabh0085@users.noreply.github.com>
2024-04-05 11:23:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
 //your JS code here. If required.
+document.addEventListener("DOMContentLoaded", function() {
+    var changeButton = document.getElementById("change_button");
+    var resetButton = document.getElementById("reset_button");
+
+    // Function to change color of grid item
+    function changeColor() {
+        var blockId = document.getElementById("block_id").value;
+        var color = document.getElementById("colour_id").value;
+
+        var gridItem = document.getElementById(blockId);
+        if (gridItem) {
+            gridItem.style.backgroundColor = color;
+        }
+    }
+
+    // Function to reset all grid item colors to transparent
+    function resetColors() {
+        var gridItems = document.getElementsByClassName("grid-item");
+        for (var i = 0; i < gridItems.length; i++) {
+            gridItems[i].style.backgroundColor = "transparent";
+        }
+    }
+
+    // Event listeners for buttons
+    changeButton.addEventListener("click", function() {
+        changeColor();
+    });
+
+    resetButton.addEventListener("click", function() {
+        resetColors();
+    });
+});
